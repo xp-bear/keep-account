221fc800cd2cc637029a0ca16b969f018468cf5e
company <1693889638@qq.com>
2025-01-16 16:51:16 +0800

    bug修复2

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -265,11 +265,11 @@ export default {
         // 第二列
         {
           values: ["2025", "2026", "2027", "2028", "2029", "2030"],
-          defaultIndex: 1,
+          defaultIndex: 0,
         },
         {
           values: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
-          defaultIndex: 1,
+          defaultIndex: 0,
         },
       ],
       incomeColumns: [
